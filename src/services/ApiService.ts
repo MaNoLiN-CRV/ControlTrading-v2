c0b16d5d95cbc845a077e6755e4f063ba87bf760
MaNoLiN-CRV <manoliin.27@gmail.com>
2025-03-16 22:43:30 +0100

    [ Api ] Add ApiService for handling API requests related to licenses, clients, and products

---
+++ src/services/ApiService.ts
@@ -0,0 +1,26 @@
+import ApiFactory from "@/fetcher/ApiFactory";
+import Licence from "@/entities/Licence";
+import Client from "@/entities/Client";
+import Product from "@/entities/Product";
+
+class ApiService {
+  private api = ApiFactory.createApiFactory("Fetch", "https://ExpertLimsApi");
+
+  async getLicenses(): Promise<Licence[]> {
+    const response = await this.api.get<Licence[]>("/licenses");
+    return response.data;
+  }
+
+  async getClients(): Promise<Client[]> {
+    const response = await this.api.get<Client[]>("/clients");
+    return response.data;
+  }
+
+  async getProducts(): Promise<Product[]> {
+    const response = await this.api.get<Product[]>("/products");
+    return response.data;
+  }
+
+}
+
+export default new ApiService();
