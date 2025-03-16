492b935975abe151fb29e8a4e85abe1ee7e571b6
MaNoLiN-CRV <manoliin.27@gmail.com>
2025-03-16 23:25:26 +0100

    [ Hooks ] Add useEventCallback hook for stable event handling in React components

---
+++ src/hooks/useEventCallback.ts
@@ -0,0 +1,10 @@
+import { useRef, useEffect, useCallback } from "react";
+
+const useEventCallback = (fn: Function) => {
+  const ref = useRef(fn);
+  useEffect(() => {
+    ref.current = fn;
+  }, [fn]);
+
+  return useCallback((...args : any) => ref.current(...args), []);
+}
