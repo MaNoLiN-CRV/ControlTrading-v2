062d9dce4f39d3a7a692c8418f21df25aa79d8cf
MaNoLiN-CRV <manoliin.27@gmail.com>
2025-03-16 17:11:46 +0100

    [ Login ] Add redirect from root to login and navigate to dashboard on authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-import { useEffect } from "react"
-import ApiFactory from "./fetcher/ApiFactory"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import ApiFactory from "./fetcher/ApiFactory";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "@/pages/login/Login";
 import ProtectedRoute from "./components/wrappers/ProtectedRoute";
 import Dashboard from "./pages/dashboard/Dashboard";
 
-
 function App() {
   useEffect(() => {
-    ApiFactory.createApiFactory("Fetch", "https://ExpertLimsApi")
+    ApiFactory.createApiFactory("Fetch", "https://ExpertLimsApi");
     return () => {
-      console.log("App unmounted")
-    }
-  }, [])
+      console.log("App unmounted");
+    };
+  }, []);
+
   return (
     <Router>
       <Routes>
+        {/* Redirect root ("/") to login */}
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route
           path="/dashboard"
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,10 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "./context/AuthContext";
 
 const Login = () => {
-  const { login, loading, error } = useAuthContext();
+  const { login, loading, error, isAuthenticated } = useAuthContext();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuthenticated()) {
+      navigate("/dashboard");
+    }
+  }, [isAuthenticated, navigate]);
 
   // Handle login
   const handleLogin = async () => {
