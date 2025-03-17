f17416dce03d307e382d068bdcfb84096723978c
ExpertLIMS <78325726+expertlims@users.noreply.github.com>
2025-03-17 20:21:54 +0100

    Create mt4licences2.sql

---
+++ sql/mt4licences2.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `mt4licences2` (
+  `idLicence` int NOT NULL AUTO_INCREMENT,
+  `MT4ID` varchar(100) NOT NULL,
+  `idProduct` int NOT NULL,
+  `idShop` int NOT NULL,
+  PRIMARY KEY (`idLicence`)
+)
