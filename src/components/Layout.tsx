d73dff84497ece2c182b5f3496b25824f7822b34
MaNoLiN-CRV <manoliin.27@gmail.com>
2025-03-27 11:00:11 +0100

    Hide Navbar on login page; adjust main padding conditionally based on page location

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,19 @@
 import { ReactNode } from 'react';
 import Navbar from './Navbar';
+import { useLocation } from 'react-router-dom';
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
+  const location = useLocation();
+  const isLoginPage = location.pathname === '/login';
+
   return (
     <div className="min-h-screen">
-      <Navbar />
-      <main className="pt-16">
+      {!isLoginPage && <Navbar />}
+      <main className={isLoginPage ? '' : 'pt-16'}>
         {children}
       </main>
     </div>
